72100d3ff6f12eac6f9c41a3330a6933699bb50f
Luke Kelly <luke.kelly141295@gmail.com>
2020-01-22 21:28:25 +0000

    completion of main script, markdown images

--- AR_Main.py
+++ AR_Main.py
@@ -22,16 +22,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Upload a python file with some useful functions
+# upload a python file with some useful functions
 from google.colab import files
 uploaded = files.upload()
 
 from ARHelperFunctions import *
 
-series = CreateSeries(length=200, random_noise=0.1)
+series = CreateSeries(length=180, random_noise=0.05)
 
 # plot noisy series
 plt.plot(series)
+plt.title("Noisy Auto-Regressive Series")
 plt.show()
 
 X_train, Y_train, X_test, Y_test = BuildDataset(series, num_lags=10)
@@ -41,9 +42,29 @@ model = ModelBuild(T=10)
 model.compile(loss="mse", optimizer=Adam(lr=0.1))
 
 training_logs = model.fit(X_train, Y_train, 
-                          epochs=30, validation_data=(X_test, Y_test),
+                          epochs=200, validation_data=(X_test, Y_test),
                           verbose=0)
 
-plt.plot(training_logs.history["loss"], label="loss")
-plt.plot(training_logs.history["val_loss"], label="val_loss")
+# plot training logs
+plt.plot(training_logs.history["loss"], label="Loss")
+plt.plot(training_logs.history["val_loss"], label="Validation Loss")
+plt.title("Training Logs")
+plt.legend()
+
+validation_target = Y_test
+
+incorrect_validation_predictions = IncorrectForecast(Y_test, X_test, model)
+
+# plot results of incorrect forecasting method
+plt.plot(validation_target, label="Target")
+plt.plot(incorrect_validation_predictions, label="Prediction")
+plt.title("Incorrect Forecast")
+plt.legend()
+
+correct_validation_predictions = CorrectForecast(Y_test, X_train, model)
+
+# plot results of correct forecasting method
+plt.plot(validation_target, label="Target")
+plt.plot(correct_validation_predictions, label="Prediction")
+plt.title("Correct Forecast")
 plt.legend()
